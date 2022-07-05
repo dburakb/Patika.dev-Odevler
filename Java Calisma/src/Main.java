e77a0a3526c97d38fdfdb0785d10bbfafece91ff
dburakb <104321702+dburakb@users.noreply.github.com>
2022-07-05 20:44:27 +0300

    Main.java
    New homework added.

--- Java Calisma/src/Main.java
+++ Java Calisma/src/Main.java
@@ -2,36 +2,60 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int mat,fizik,kimya,turkce,muzik;
-        double ortalama;
+        int mat=0,fizik=0,kimya=0,turkce=0,muzik=0,a,b,c,d,e;
+        double ortalama, dersSayisi = 0.0;
         String sonuc;
 
         Scanner inp = new Scanner(System.in);
 
         System.out.print("Matematik notunuz: ");
-        mat=inp.nextInt();
+        a=inp.nextInt();
+        if (a<=100&&a>=0){
+            mat=a;
+            dersSayisi++;
+        }
         System.out.print("\n");
 
         System.out.print("Fizik notunuz: ");
-        fizik=inp.nextInt();
+        b=inp.nextInt();
+        if (b<=100&&b>=0){
+            fizik=b;
+            dersSayisi++;
+        }
         System.out.print("\n");
 
         System.out.print("Kimya notunuz: ");
-        kimya=inp.nextInt();
+        c=inp.nextInt();
+        if (c<=100&&c>=0){
+            kimya=c;
+            dersSayisi++;
+        }
         System.out.print("\n");
 
         System.out.print("Türkçe notunuz: ");
-        turkce=inp.nextInt();
+        d=inp.nextInt();
+        if (d<=100&&d>=0){
+            turkce=d;
+            dersSayisi++;
+        }
         System.out.print("\n");
 
         System.out.print("Müzik notunuz: ");
-        muzik=inp.nextInt();
+        e=inp.nextInt();
+        if (e<=100&&e>=0){
+            muzik=e;
+            dersSayisi++;
+        }
         System.out.print("\n");
 
-        ortalama=(mat+fizik+kimya+turkce+muzik)/5.0;
+        ortalama=(mat+fizik+kimya+turkce+muzik)/dersSayisi;
         System.out.println("Ortalamanız: " + ortalama);
 
-        sonuc = (ortalama > 60) ? "Geçtin" : "Kaldın";
-        System.out.println(sonuc);
+        if (ortalama>=55){
+            System.out.println("Tebrikler! Sınıfı geçtiniz.");
+        }
+        else {
+            System.out.println("Maalesef sınıfı geçemediniz. Seneye tekrar görüşmek üzere!");
+        }
     }
 }
