f8164e70b7af1e10582f9878aa35dcd3affd8083
dburakb <104321702+dburakb@users.noreply.github.com>
2022-08-26 21:24:08 +0300

    YıldızUcgen.java
    New homework added.

---
+++ Java Calisma/src/YıldızUcgen.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class YıldızUcgen {
+    public static void main(String[] args) {
+        int n,a,b,c;
+        Scanner inp = new Scanner(System.in);
+        System.out.print(" Bir sayı gırınız : ");
+        n = inp.nextInt();
+        for ( a = 1; a <= n; a++) {
+            for ( b = 1; b <= (n - a); b++) {
+                System.out.print(" ");
+
+            }
+            for ( c = 1; c <= ( 2* a - 1 ); c++) {
+                System.out.print("*");
+
+            }
+            System.out.println(" ");
+
+        }
+        for ( a = 1; a < n; a++) {
+            for ( b = 1; b <= a; b++) {
+                System.out.print(" ");
+
+            }
+            for ( c = 1; c <= ( 2 * n - (2 * a + 1) ); c++) {
+                System.out.print("*");
+
+            }
+            System.out.println(" ");
+
+        }
+
+    }
+}
